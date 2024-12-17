bea05073d8d827128b3cab83b27ffa0299ab3b9e
AlexGit40 <oleksandrkolesnyk45@gmail.com>
2024-12-17 22:58:48 +0200

    create AuthForm.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import AuthForm from "./components/AuthForm"
+
+function App() {
+  return (
+    <div className="container">
+      <h1>React 19 - Overview</h1>
+      <AuthForm/>
+    </div>
+  )
+}
+
+export default App
